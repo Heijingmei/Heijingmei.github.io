18031c0697335427b85fdf5d3faecfdaf0360d8d
Heijingmei <bh.heijingmei@gmail.com>
2024-03-27 16:31:36 +0100

    111

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -4,21 +4,6 @@ export const zhNavbar = navbar([
   "/zh/",
   "/好好好",
   {
-    text: "博文",
-    icon: "pen-to-square",
-    prefix: "/zh/posts/",
-    children: [
-      {
-        text: "苹果",
-        icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
-        ],
-      },
       {
         text: "香蕉",
         icon: "pen-to-square",
@@ -38,12 +23,6 @@ export const zhNavbar = navbar([
           "4",
         ],
       },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
-    ],
-  },
   {
     text: "V2 文档",
     icon: "book",
---
+++ src/de/program/C/note1.md
@@ -0,0 +1,27 @@
+---
+title: 01-C 语言基础概念
+date: 2023-07-16 16:09:18
+author: 黑静美
+isOriginal: true
+category: 
+    - 编程
+tag:
+    - C
+    - C#
+icon: c
+sticky: false
+star: false
+article: true
+timeline: true
+image: false
+navbar: true
+sidebarIcon: true
+headerDepth: 3
+comment: true
+lastUpdated: true
+editLink: false
+backToTop: true
+---
+
+# jdiogjf
+
