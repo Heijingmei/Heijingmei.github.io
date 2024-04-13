598cbe99f5f8d59fc33bcf910d24aeb739f6185a
Heijingmei <164376088+Heijingmei@users.noreply.github.com>
2024-04-13 14:55:07 +0000

    Deploying to gh-pages from @ Heijingmei/Encryption@3d507786b698fdcf2c8df8b5945ea95b2138923d 🚀

---
+++ assets/pageview-BapqPYSz.js
@@ -0,0 +1,1 @@
+import{_ as o,u as n}from"./app-Cfrpy3CN.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-Cfrpy3CN.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
