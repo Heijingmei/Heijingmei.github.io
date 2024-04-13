4bb8a2f07149aca91eedc492af51a33e0147d2a2
Heijingmei <164376088+Heijingmei@users.noreply.github.com>
2024-04-13 15:54:27 +0000

    Deploying to gh-pages from @ Heijingmei/Encryption@001d0d217c3a8e38595db36b797eb2e244959dce 🚀

---
+++ assets/pageview-CYdmSta-.js
@@ -0,0 +1,1 @@
+import{_ as o,u as n}from"./app-CjdBT6CV.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-CjdBT6CV.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
