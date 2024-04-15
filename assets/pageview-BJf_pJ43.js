125f78dfe47b07b930ddd824f5d644bed96715da
Heijingmei <164376088+Heijingmei@users.noreply.github.com>
2024-04-15 16:11:43 +0000

    Deploying to gh-pages from @ Heijingmei/Encryption@bcfaf61d2b9fe6980f498b3ee32857a19ac74f58 🚀

--- assets/pageview-BJf_pJ43.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,u as n}from"./app-CFx_4KlG.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-CFx_4KlG.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
