d09beba082158ed974dcb1aa90705a60d0706ea5
Heijingmei <164376088+Heijingmei@users.noreply.github.com>
2024-04-17 13:41:31 +0000

    Deploying to gh-pages from @ Heijingmei/Encryption@2e68e54e7788ebe5829dfc64dce64e70096d851d 🚀

---
+++ assets/Untitled.html-C9Uqa1Ni.js
@@ -0,0 +1,18 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as i,c as t,e as n}from"./app-deHhH_ih.js";const a={},r=n(`<div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token function">git</span> config <span class="token parameter variable">--local</span> user.name &quot;github的用户名“
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><div class="language-我也不知道算啥 line-numbers-mode" data-ext="我也不知道算啥" data-title="我也不知道算啥"><pre class="language-我也不知道算啥"><code>[core]
+	repositoryformatversion = 0
+	filemode = true
+	bare = false
+	logallrefupdates = true
+	ignorecase = true
+	precomposeunicode = true
+[remote &quot;origin&quot;]
+	url = https://github.com/用户名/仓库.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[user]
+	name = Heijingmei
+	email = bh.heijingmei@gmail.com
+[branch &quot;main&quot;]
+	remote = origin
+	merge = refs/heads/main
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),s=[r];function l(o,d){return i(),t("div",null,s)}const u=e(a,[["render",l],["__file","Untitled.html.vue"]]),g=JSON.parse('{"path":"/zh/Lilya/program/GitHub%E7%9B%B8%E5%85%B3/Untitled.html","title":"","lang":"zh-CN","frontmatter":{"head":[["meta",{"property":"og:url","content":"https://github.com/Heijingmei/Heijingmei.github.io/zh/Lilya/program/GitHub%E7%9B%B8%E5%85%B3/Untitled.html"}],["meta",{"property":"og:site_name","content":"BH工作室"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-04-17T13:40:00.000Z"}],["meta",{"property":"article:modified_time","content":"2024-04-17T13:40:00.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-04-17T13:40:00.000Z\\",\\"author\\":[]}"]]},"headers":[],"git":{"createdTime":1713361200000,"updatedTime":1713361200000,"contributors":[{"name":"Heijingmei","email":"bh.heijingmei@gmail.com","commits":1}]},"readingTime":{"minutes":0.19,"words":56},"filePathRelative":"zh/Lilya/program/GitHub相关/Untitled.md","localizedDate":"2024年4月17日","excerpt":"<div class=\\"language-bash\\" data-ext=\\"sh\\" data-title=\\"sh\\"><pre class=\\"language-bash\\"><code><span class=\\"token function\\">git</span> config <span class=\\"token parameter variable\\">--local</span> user.name \\"github的用户名“\\n</code></pre></div><div class=\\"language-我也不知道算啥\\" data-ext=\\"我也不知道算啥\\" data-title=\\"我也不知道算啥\\"><pre class=\\"language-我也不知道算啥\\"><code>[core]\\n\\trepositoryformatversion = 0\\n\\tfilemode = true\\n\\tbare = false\\n\\tlogallrefupdates = true\\n\\tignorecase = true\\n\\tprecomposeunicode = true\\n[remote \\"origin\\"]\\n\\turl = https://github.com/用户名/仓库.git\\n\\tfetch = +refs/heads/*:refs/remotes/origin/*\\n[user]\\n\\tname = Heijingmei\\n\\temail = bh.heijingmei@gmail.com\\n[branch \\"main\\"]\\n\\tremote = origin\\n\\tmerge = refs/heads/main\\n</code></pre></div>"}');export{u as comp,g as data};
